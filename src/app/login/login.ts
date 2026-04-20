0d6d23c0dcdf2c37971e2087e4151e106e821715
karthi-kn04 <karthikn200412@gmail.com>
2026-04-20 21:01:39 +0530

    login

--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -1,9 +1,32 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, ValidationErrors, Validators, AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-export class Login {}
+export class Login {
+
+
+  loginForm = new FormGroup({
+    userName: new FormControl('', [Validators.required, Validators.minLength(5),this.noSpace]),
+    password: new FormControl('', [Validators.required, Validators.minLength(6)])
+  });
+
+  noSpace(control: AbstractControl): ValidationErrors | null {
+    let value = control.value ;
+    if (value && typeof value === 'string' && value.includes(' ')) {
+      return { noSpaceError: true };
+    }
+    return null;
+  }
+  onSubmit() {
+    
+      console.log(this.loginForm.value);
+      
+    }
+    
+  }
+
