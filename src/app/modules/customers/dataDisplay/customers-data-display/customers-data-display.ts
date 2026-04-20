ce12110c598f7c5118a5a55b170dcccb705a5dc1
abinaya <abinayakayal2004@gmail.com>
2026-04-20 15:53:08 +0530

    creating the component ans folder

--- src/app/modules/customers/dataDisplay/customers-data-display/customers-data-display.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-customers-data-display',
-  imports: [],
-  templateUrl: './customers-data-display.html',
-  styleUrl: './customers-data-display.css',
-})
-export class CustomersDataDisplay {}
