468b25cc7d226eafeef82f72d3258b19f3651377
abinaya <abinayakayal2004@gmail.com>
2026-04-21 20:48:41 +0530

    commiting the endpoints

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,12 +2,14 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CustomersApiDisplay } from './modules/customers/customers-api-display/customers-api-display';
 import { Login } from './login/login';
-
+import { ProductsDataPut } from './modules/products/dataDisplay/products-data-put/products-data-put';
+import { CustomersDataPut } from "./modules/customers/dataDisplay/customers-data-put/customers-data-put";
+import { CustomersDataDelete } from "./modules/customers/dataDisplay/customers-data-delete/customers-data-delete";
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CustomersApiDisplay, Login],
+  imports: [RouterOutlet, CustomersApiDisplay, Login, ProductsDataPut, CustomersDataPut, CustomersDataDelete],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- src/app/modules/customers/customers-api-display/customers-api-display.ts
+++ src/app/modules/customers/customers-api-display/customers-api-display.ts
@@ -1,13 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterOutlet } from "../../../../../node_modules/@angular/router/types/_router_module-chunk";
 
 @Component({
   selector: 'app-customers-api-display',
-  imports: [],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './customers-api-display.html',
   styleUrl: './customers-api-display.css',
 })
 export class CustomersApiDisplay {
-submit(){
-   
-}
+ endpoints = [
+    { name: 'Get All Customers', method: 'GET', route: 'get-all', desc: 'Get all customer details from system database' },
+    { name: 'Get Customer By ID', method: 'GET', route: 'get-by-customerid', desc: 'Get customer details using given customer ID' },
+    { name: 'Get Customer By Email', method: 'GET', route: 'get-by-email', desc: 'Get customer details using given email address' },
+    { name: 'Create Customer', method: 'POST', route: 'create', desc: 'Create new customer by providing required details' },
+    { name: 'Update Customer', method: 'PUT', route: 'update', desc: 'Update existing customer details using customer ID' },
+    { name: 'Delete Customer', method: 'DELETE', route: 'delete', desc: 'Delete customer record using given customer ID' },
+    { name: 'Get Customer Orders', method: 'GET', route: 'get-orders', desc: 'Get all orders placed by customer using customer ID' },
+    { name: 'Get Customer Shipments', method: 'GET', route: 'get-shipments', desc: 'Get all shipments associated with customer using customer ID' }
+    
+  ];
 }
