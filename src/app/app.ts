179840cbc28e6b4ae47e63ec0c81bbb0d54ea0bc
FrontendCode <nilani@email.com>
2026-04-22 08:06:42 +0530

    frontend

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,12 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CustomersApiDisplay } from './modules/customers/customers-api-display/customers-api-display';
 import { Login } from './login/login';
+import { OrdersDataDelete } from './modules/orders/dataDisplay/orders-data-delete/orders-data-delete';
+import { OrderItemsDataDelete } from './modules/ordeItems/dataDisplay/order-items-data-delete/order-items-data-delete';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CustomersApiDisplay, Login],
+  imports: [RouterOutlet, CustomersApiDisplay, Login, OrdersDataDelete,OrderItemsDataDelete],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
